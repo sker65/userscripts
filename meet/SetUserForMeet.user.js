e5352b480f0fe2a8abc9287c3e58bd61b21c501d
Stefan Rinke <sker65@gmail.com>
2021-12-15 11:57:13 +0100

    - added some meta tags

--- meet/SetUserForMeet.user.js
+++ meet/SetUserForMeet.user.js
@@ -3,7 +3,8 @@
 // @namespace    https://github.com/sker65/userscripts/tree/main/meet
 // @version      0.1
 // @description  Always switch to authuser=1 if a blank meeting link is provided
-// @author       You
+// @updateURL    https://github.com/sker65/userscripts/raw/main/meet/SetUserForMeet.user.js
+// @author       Stefan Rinke
 // @match        https://meet.google.com/*
 // @icon         https://cdn.icon-icons.com/icons2/2642/PNG/512/google_meet_camera_logo_icon_159349.png
 // @grant        none
